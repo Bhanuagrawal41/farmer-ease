048b05c7401af015a067329fd86d53ab8538f7c1
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2025-09-30 11:24:10 +0530

    fix: Correct casing for all component imports

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import Header from "./Header";
-import HeroSection from "./HeroSection";
+// import Header from "./Header";
+import Header from "./Header.jsx";
+// import HeroSection from "./HeroSection";
+import HeroSection from "./HeroSection.jsx";
 // import WhyChoose from "./WhyChoose";
 import WhyChoose from './whyChoose.jsx'
-import OurFeatures from "./OurFeatures";
-import GetInTouch from "./GetinTouch";
-import ChatBot from "./chatBot";
+import OurFeatures from "./OurFeatures.jsx";
+import GetInTouch from "./GetinTouch.jsx";
+// import ChatBot from "./chatBot";
+import ChatBot from './ChatBot.jsx';
 
 function MainPage() {
   return (
